4c6d83d924eb30b2716eb53f8f0e9f8eec53b672
Weiwen Chou <wc2937@columbia.edu>
2026-04-15 02:27:53 +0000

    deleted fivetran_id, fivefran_delete, and fivetran_synced

--- models/staging/web/base_page_views.sql
+++ models/staging/web/base_page_views.sql
@@ -1,8 +1,5 @@
 SELECT
-    "_fivetran_id" AS _fivetran_id,
     SESSION_ID AS SESSION_ID,
     PAGE_NAME AS PAGE_NAME,
-    CAST(VIEW_AT AS TIMESTAMP) AS VIEW_AT,
-    "_fivetran_deleted" AS IS_DELETED,
-    CAST("_fivetran_synced" AS TIMESTAMP) AS SYNCED_AT
+    CAST(VIEW_AT AS TIMESTAMP) AS PAGE_VIEW_AT,
 FROM {{ source('web', 'page_views') }}
